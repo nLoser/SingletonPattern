549fb2fc54f6b05275590ff705cef5bfd7b6e8ad
Ross <954777574@qq.com>
2016-04-06 14:28:45 +0800

    Initial Commit

---
+++ SingletonPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SingletonPattern
+//
+//  Created by LV on 16/4/6.
+//  Copyright © 2016年 Wieye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
